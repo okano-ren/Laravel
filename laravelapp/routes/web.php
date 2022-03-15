de1a30d9167c414ca7963ac39dbe45ffbc4013e1
okano-ren <ren20021221@icloud.com>
2022-03-15 12:55:55 +0900

    3/15

--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -28,6 +28,15 @@ Route::get('hello','HelloController@index');
 
 Route::post('hello','HelloController@post');
 
+Route::get('hello/add','HelloController@add');
+Route::post('hello/add','HelloController@create');
+
+Route::get('hello/edit','HelloController@edit');
+Route::get('hello/edit','HelloController@update');
+
+Route::get('hello/del','HelloController@del');
+Route::post('hello/del','HelloController@remove');
+
 
 // 実習問題用
 // 2-1
